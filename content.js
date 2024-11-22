8837a0f94d503d8d5589eee9cb1ae0fc78050824
nazeeb <nazeebindian@gmail.com>
2024-11-22 09:09:13 +0530

    products displayed

--- content.js
+++ content.js
@@ -1,20 +1,13 @@
-// console.clear();
 
 let contentTitle;
 
-console.log(document.cookie);
 function dynamicClothingSection(ob) {
   let boxDiv = document.createElement("div");
   boxDiv.id = "box";
 
   let boxLink = document.createElement("a");
-  // boxLink.href = '#'
   boxLink.href = "/contentDetails.html?" + ob.id;
-  // console.log('link=>' + boxLink);
-
   let imgTag = document.createElement("img");
-  // imgTag.id = 'image1'
-  // imgTag.id = ob.photos
   imgTag.src = ob.preview;
 
   let detailsDiv = document.createElement("div");
@@ -42,101 +35,43 @@ function dynamicClothingSection(ob) {
   return boxDiv;
 }
 
-//  TO SHOW THE RENDERED CODE IN CONSOLE
-// console.log(dynamicClothingSection());
-
-// console.log(boxDiv)
-
 let mainContainer = document.getElementById("mainContainer");
 let containerClothing = document.getElementById("containerClothing");
 let containerAccessories = document.getElementById("containerAccessories");
-// mainContainer.appendChild(dynamicClothingSection('hello world!!'))
 
-// BACKEND CALLING
-
-let httpRequest = new XMLHttpRequest();
-
+const productsArray = [];
 async function productsListApi() {
   const SHEET_ID = "1X9MNBQpWpv8wlLJrmZ133TQ8REO9s1OHHiYS1_bzlvQ";
   const GID = "1144256468";
   const QUERY = `SELECT * `;
   const res = await readGsheetData(SHEET_ID, GID, QUERY);
-  const columns= [...res?.table?.cols];
-  console.log("===res", columns, res?.table?.rows?.map((item)=>({...columns?.map((header, i)=>(item?.c?.[i]?.v))})));
-}
-
-
-productsListApi();
-
-const responseObject = [
-  {
-    id: "1",
-    name: "Roasted Cashew",
-    preview: "img/img5.png",
-    // "preview": "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg",
-    photos: [
-      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg",
-      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/efc3d5b9-1bb3-4427-af53-7acae7af98951541402833591-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-2.jpg",
-      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/c7e58861-3431-4189-9903-9880f5eebd181541402833566-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-3.jpg",
-      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/66490b64-32de-44b4-a6e4-fe36f1c040051541402833548-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-4.jpg",
-      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/957be784-7c5d-4e90-ab9f-0928015b22891541402833645-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-5.jpg",
-    ],
-    description: "Roasted",
-    size: [1, 1, 0, 1, 0],
-    isAccessory: false,
-    brand: "500G",
-    price: 450,
-  },
-  {
-    id: "1",
-    name: "Roasted Cashew",
-    preview: "img/img5.png",
-    // "preview": "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg",
-    photos: [
-      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg",
-      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/efc3d5b9-1bb3-4427-af53-7acae7af98951541402833591-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-2.jpg",
-      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/c7e58861-3431-4189-9903-9880f5eebd181541402833566-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-3.jpg",
-      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/66490b64-32de-44b4-a6e4-fe36f1c040051541402833548-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-4.jpg",
-      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/957be784-7c5d-4e90-ab9f-0928015b22891541402833645-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-5.jpg",
-    ],
-    description: "Roasted",
-    size: [1, 1, 0, 1, 0],
-    isAccessory: false,
-    brand: "500G",
-    price: 450,
-  },
-];
-
-httpRequest.onreadystatechange = function () {
-  if (this.readyState === 4) {
-    if (this.status == 200) {
-      // console.log('call successful');
-      // contentTitle = JSON.parse(this.responseText);
-      contentTitle = responseObject;
-      if (document.cookie.indexOf(",counter=") >= 0) {
-        var counter = document.cookie.split(",")[1].split("=")[1];
-        document.getElementById("badge").innerHTML = counter;
+  const columns = [...res?.table?.cols];
+  res?.table?.rows?.map((item) => {
+    const productObj = {};
+    columns?.map((header, i) => {
+      if (header?.label === "photos" || header?.label === "size") {
+        productObj[header?.label] = item?.c?.[i]?.v?.split(",");
+      } else {
+        productObj[header?.label] = item?.c?.[i]?.v;
       }
-      for (let i = 0; i < contentTitle.length; i++) {
-        if (contentTitle[i].isAccessory) {
-          containerAccessories.appendChild(
-            dynamicClothingSection(contentTitle[i])
-          );
-        } else {
-          containerClothing.appendChild(
-            dynamicClothingSection(contentTitle[i])
-          );
-        }
-      }
+      return "";
+    });
+    productsArray?.push(productObj);
+    return "";
+  });
+  if (document.cookie.indexOf(",counter=") >= 0) {
+    var counter = document.cookie.split(",")[1].split("=")[1];
+    document.getElementById("badge").innerHTML = counter;
+  }
+  for (let i = 0; i < productsArray?.length; i++) {
+    if (productsArray?.[i].isAccessory) {
+      containerAccessories.appendChild(
+        dynamicClothingSection(productsArray?.[i])
+      );
     } else {
-      console.log("call failed!");
+      containerClothing.appendChild(dynamicClothingSection(productsArray?.[i]));
     }
   }
-};
-httpRequest.open(
-  "GET",
-  "https://5d76bf96515d1a0014085cf9.mockapi.io/product",
-  true
-);
-httpRequest.send();
+}
 
+productsListApi();
